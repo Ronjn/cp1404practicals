87ff6ed4056ee9901a4f336acef6095523264c18
Bryn <brynbaird@protonmail.com>
2023-06-07 19:50:47 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,27 @@
-pwd = input("Enter password: ")
-i = len(pwd)
-while i < 6:
-    print("Password needs to be a minimum of 6 characters")
+def main():
+    i = get_password()
+    while i < 6:
+        print("Password needs to be a minimum of 6 characters")
+        i = get_password()
+    print("Password is: ", end='')
+    print_asterisk(i)
+
+
+def print_asterisk(i):
+    for j in range(0, i, 1):
+        print("*", end='')
+    print()
+
+
+def get_password():
     pwd = input("Enter password: ")
     i = len(pwd)
+    return i
 
-print("Password is: ", end='')
 
-for j in range(0, i, 1):
-    print("*", end='')
-print()
+main()
+
+
+
+
+
