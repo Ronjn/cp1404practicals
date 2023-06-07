9c6d111afaeec3e410c2c8194cb9068d883676a5
Bryn <brynbaird@protonmail.com>
2023-06-07 20:24:15 +1000

    Refactor temperature and score programs with main function

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -3,35 +3,38 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
-
-def celcius_to_fahrenheit():
-    global celsius
-    celsius = 5 / 9 * (fahrenheit - 32)
-
-
-def fahrenheit_to_celcius():
-    global fahrenheit
-    fahrenheit = celsius * 9.0 / 5 + 32
-
-
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit_to_celcius()
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        fahrenheit = float(input("Fahrenheit: "))
-        celcius_to_fahrenheit()
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+def main():
+    MENU = """    C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(celsius, fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def fahrenheit_to_celsius(celsius, fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+main()
+
