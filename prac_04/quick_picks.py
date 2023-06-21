c103f67f98e4d34d9d2f9c623de114dfb149968d
Bryn <brynbaird@protonmail.com>
2023-06-22 01:49:51 +1000

    Adds finished quickpick code

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,26 +1,17 @@
 import random
 lines = []
-string_lines = []
+
 
 def main():
     quickpick_count = int(input("How many quick picks? :"))
-
     for i in range(0, quickpick_count):
         line = []
         for j in range(0, 6):
             line.append(random.randint(1, 45))
+        line.sort()
         lines.append(line)
-
-    convert_int_to_string(quickpick_count)
-
-
-def convert_int_to_string(quickpick_count):
-    for i in range(0, quickpick_count):
-        string_line = []
-        for j in range(0, 6):
-            string_line = [str(number) for number in lines[i]]
-        string_lines.append(string_line)
-        print(" ".join(sorted(string_lines[i])))
+        print("{:2}  {:2}  {:2}  {:2}  {:2}  {:2}".format(lines[i][0], lines[i][1], lines[i][2], lines[i][3],
+                                                          lines[i][4], lines[i][5]))
 
 
 main()
