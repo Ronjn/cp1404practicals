34b14807f5906bc230644ce9dee588d51ea9b133
Bryn <brynbaird@protonmail.com>
2023-06-30 14:55:18 +1000

    Fix declaration of variables

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,15 +1,15 @@
 
 
-numbers = []
-number_count = 5
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-
 def main():
+    numbers = []
+    number_count = 5
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn',
+                 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
     for number in range(1, number_count + 1):
         number = float(input(f"Enter number {number} : "))
         numbers.append(number)
-    calculations()
+    calculations(numbers)
 
     user = input("Enter username: ")
     if user in usernames:
@@ -18,11 +18,7 @@ def main():
         print("Access Denied")
 
 
-
-
-
-
-def calculations():
+def calculations(numbers):
     small_num = min(numbers)
     big_num = max(numbers)
     avg_num = sum(numbers) / len(numbers)
