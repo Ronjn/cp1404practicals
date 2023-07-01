cd17e6d8e27f9a757223632283252a6a05dc9d54
Bryn <brynbaird@protonmail.com>
2023-07-01 22:05:35 +1000

    Adds sorting of the dictionary before printing

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -9,9 +9,14 @@ words = text.split()
 unique_words = set(words)
 word_to_frequency = {}
 
+# Adds values to the dictionary for each unique word from the user input
 for word in unique_words:
     word_to_frequency[word] = words.count(word)
 
-max_length = max(len(word) for word in unique_words)
+word_to_frequency = dict(sorted(word_to_frequency.items()))  # Sorts the dictionary by alphabetical order
+
+max_length = max(len(word) for word in unique_words)  # Sets the max length to the length of the longest word
+
+# Prints the word and frequency for each word in the dictionary
 for key in word_to_frequency:
     print(f"{key:{max_length}} = {word_to_frequency[key]}")
