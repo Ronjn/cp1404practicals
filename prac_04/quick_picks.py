447f0a107238d9dd85a166c4df05cc9325b98ccf
Bryn <brynbaird@protonmail.com>
2023-07-01 15:10:22 +1000

    Fix vars to constants

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,21 +1,21 @@
 import random
 
+NUMBER_OF_SELECTIONS = 6    # How many numbers per quick pick
+CANDIDATE_MIN = 1           # lowest number for selection
+CANDIDATE_MAX = 45          # highest number for selection
+
 
 def main():
-    number_of_selections = 6    # How many numbers per quick pick
-    candidate_min = 1           # lowest number for selection
-    candidate_max = 45          # highest number for selection
-
     # Ask the user how many quick picks to generate
     number_of_picks = int(input("How many quick picks? :"))
 
     # Generates and outputs each pick
     for pick in range(0, number_of_picks):
         selections = []
-        candidates = list(range(candidate_min, candidate_max + 1))  # Creates a list of all possible candidates
+        candidates = list(range(CANDIDATE_MIN, CANDIDATE_MAX + 1))  # Creates a list of all possible candidates
 
         # Select as many candidates as required
-        for selection_index in range(0, number_of_selections):
+        for selection_index in range(0, NUMBER_OF_SELECTIONS):
             selection = random.choice(candidates)   # Selects a random number from candidates
             selections.append(selection)            # Adds that number to the selections
             candidates.remove(selection)            # Removes that number from candidates
