4e329f289ba31db9a3506d160684a4d678fe850a
Bryn <brynbaird@protonmail.com>
2023-07-01 17:43:29 +1000

    Add code to colour dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+CODE_TO_COLOUR = {"Red": "#ff0000", "Green": "#00ff00", "Blue": "#0000ff", "Orange": "#ffa500",
+                  "Yellow": "#ffff00", "White": "#ffffff", "Pink": "#ffb5c5", "Purple": "#9b30ff",
+                  "Black": "#000000", "Aqua": "#00ffff"}
