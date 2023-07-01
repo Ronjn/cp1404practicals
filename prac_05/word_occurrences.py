b78176de0311323b81a9bd4bdd1467d27db12c1f
Bryn <brynbaird@protonmail.com>
2023-07-01 21:57:14 +1000

    Add dictionary for text and print function

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,3 +4,14 @@ Estimate: 25 minutes
 Actual:    minutes
 """
 
+text = input("Enter your text: ")
+words = text.split()
+unique_words = set(words)
+word_to_frequency = {}
+
+for word in unique_words:
+    word_to_frequency[word] = words.count(word)
+
+max_length = max(len(word) for word in unique_words)
+for key in word_to_frequency:
+    print(f"{key:{max_length}} = {word_to_frequency[key]}")
