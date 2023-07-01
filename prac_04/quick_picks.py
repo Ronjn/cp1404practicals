b6c494010b83e114112ab1eec049405270563701
Bryn <brynbaird@protonmail.com>
2023-07-01 13:54:49 +1000

    Fix naming convention for line_of_numbers list

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,13 +5,13 @@ def main():
     lines = []
     number_of_quickpicks = int(input("How many quick picks? :"))
     for i in range(0, number_of_quickpicks):
-        line = []
+        line_of_numbers = []
         number_of_columns = 6
         range_of_numbers = 45
         for j in range(0, number_of_columns):
-            line.append(random.randint(1, range_of_numbers))
-        line.sort()
-        lines.append(line)
+            line_of_numbers.append(random.randint(1, range_of_numbers))
+        line_of_numbers.sort()
+        lines.append(line_of_numbers)
         print("{:2}  {:2}  {:2}  {:2}  {:2}  {:2}".format(lines[i][0], lines[i][1], lines[i][2], lines[i][3],
                                                           lines[i][4], lines[i][5]))
 
