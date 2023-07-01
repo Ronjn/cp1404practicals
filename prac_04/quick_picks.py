530286aee457809c14fc8e060a995f0ed492c8d3
Bryn <brynbaird@protonmail.com>
2023-07-01 14:27:20 +1000

    Refactored output loop

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -2,18 +2,20 @@ import random
 
 
 def main():
-    lines = []
-    number_of_quickpicks = int(input("How many quick picks? :"))
-    for i in range(0, number_of_quickpicks):
-        line_of_numbers = []
-        number_of_columns = 6
-        range_of_numbers = 45
-        for j in range(0, number_of_columns):
-            line_of_numbers.append(random.randint(1, range_of_numbers))
-        line_of_numbers.sort()
-        lines.append(line_of_numbers)
-        print("{:2}  {:2}  {:2}  {:2}  {:2}  {:2}".format(lines[i][0], lines[i][1], lines[i][2], lines[i][3],
-                                                          lines[i][4], lines[i][5]))
+    number_of_rows = int(input("How many quick picks? :"))
+    for i in range(0, number_of_rows):
+        selections = []
+        number_of_selections = 6
+        candidate_min = 1
+        candidate_max = 45
+        candidates = list(range(candidate_min, candidate_max + 1))  # Creates a list of candidates from 1,
+        for j in range(0, number_of_selections):
+            random_choice = random.choice(candidates)      # Selects a random number from candidates
+            candidates.remove(random_choice)               # Removes that number from candidates
+            selections.append(random_choice)        # Adds that number to the selections
+        selections.sort()
+        print("{:2}  {:2}  {:2}  {:2}  {:2}  {:2}".format(selections[0], selections[1], selections[2],
+                                                          selections[3], selections[4], selections[5]))
 
 
 main()
