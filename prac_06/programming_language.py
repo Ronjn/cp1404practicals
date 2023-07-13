bed4fdf1b3ed1337ec1a7d7b6c2c1df2762b177a
Bryn <brynbaird@protonmail.com>
2023-07-13 15:04:28 +1000

    Add completed exercises

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,4 +1,20 @@
 """
 Estimated time to complete  : 40 minutes
-Actual time to complete     :
+Actual time to complete     : 30 minutes
 """
+
+
+class ProgrammingLanguage:
+
+    def __init__(self, name="", typing="", reflection="", year=0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflections={self.reflection}, First appeared in {self.year}"
