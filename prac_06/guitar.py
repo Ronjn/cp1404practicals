d20576768c70805ade2374a8d5abd25d382db0ab
Bryn <brynbaird@protonmail.com>
2023-07-15 19:03:39 +1000

    Add missing doc strings to functions

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -10,6 +10,12 @@ VINTAGE_MIN = 50
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance.
+
+        name: string, is simply the name of the guitar
+        year: int, is the year that the guitar was made
+        cost: float, is the price of the guitar
+        """
         self.name = name
         self.year = year
         self.cost = cost
@@ -18,10 +24,12 @@ class Guitar:
         return f"{self.name}, ({self.year}) : ${self.cost}"
 
     def get_age(self):
+        """Get the age of the guitar"""
         age = YEAR - self.year
         return age
 
     def is_vintage(self):
+        """Get whether the guitar is classified as 'vintage'"""
         if self.get_age() >= VINTAGE_MIN:
             return True
         else:
