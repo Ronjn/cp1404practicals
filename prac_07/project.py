45c0b98deba11fa01eca4f8a213f8ef56d528c3c
Bryn <brynbaird@protonmail.com>
2023-07-22 18:04:13 +1000

    Add basic class structure

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,3 +2,23 @@
 Estimated time to complete  : 90 minutes
 Actual time to complete     :  minutes
 """
+
+
+class Project:
+
+    def __init__(self, name="", date="", priority=0, cost=0, completion=0):
+        """Initialise a Guitar instance.
+
+        name: string, is simply the name of the guitar
+        year: int, is the year that the guitar was made
+        cost: float, is the price of the guitar
+        """
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+    def __str__(self):
+        return f"{self.name}, started on {self.date}, priority:{self.priority}, Estimated cost: ${self.cost}," \
+               f"{self.completion}% completed"
