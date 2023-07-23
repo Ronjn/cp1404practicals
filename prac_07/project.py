59ce01ec4ac434fd769c2454e6796410b2b4c592
Bryn <brynbaird@protonmail.com>
2023-07-23 21:42:25 +1000

    Fix incorrect docstrings

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,9 +9,11 @@ class Project:
     def __init__(self, name="", date="", priority=0, cost=0, completion=0):
         """Initialise a Guitar instance.
 
-        name: string, is simply the name of the guitar
-        year: int, is the year that the guitar was made
-        cost: float, is the price of the guitar
+        name: string, is simply the name of the project
+        date: string, is start date of the project
+        priority: int, is the priority of the project
+        cost: float, is the estimated cost of the project
+        completion: int, is the percentage completed of the project
         """
         self.name = name
         self.date = date
