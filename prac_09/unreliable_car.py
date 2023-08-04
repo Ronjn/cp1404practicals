b6829ca47b9fb43b5b9d45a6c10f595217d8a6e6
Bryn <brynbaird@protonmail.com>
2023-08-04 16:29:09 +1000

    Complete UnreliableCar exercise

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,25 @@
+from prac_09.car import Car
+import random
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes reliability attribute."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive like parent Car if the reliability is higher than random number generated"""
+        random_number = random.randint(0, 100)
+        print(random_number)
+
+        if self.reliability > random_number:
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self._odometer += distance
+        return distance
---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,7 @@
+from prac_09.unreliable_car import UnreliableCar
+
+bad_car = UnreliableCar(100, "Subie", 50)
+
+bad_car.drive(50)
+
+print(bad_car)
